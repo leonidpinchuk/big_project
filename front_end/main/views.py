4cfa804fc6263c019f6f4991c09d259105d8b782
leoni <leonidpinchuk98@gmail.com>
2022-10-28 03:32:52 +1000

    + Task Nul

--- front_end/main/views.py
+++ front_end/main/views.py
@@ -3,7 +3,7 @@ from .models import Task
 
 
 def index(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.order_by('-id')
     return render(request, 'main/index.html', {'title': 'Main list', 'tasks': tasks})
 
 def about(request):
